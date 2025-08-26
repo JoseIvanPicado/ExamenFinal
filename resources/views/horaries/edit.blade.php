c76d156d18811680e79a273c83caf28dfdccbf57
ColvanVance <jose.picado8@tecnacional.edu.ni>
2025-08-26 16:01:00 -0600

    Build: Creación de la vista faltante edit en horario

--- resources/views/horaries/edit.blade.php
+++ resources/views/horaries/edit.blade.php
@@ -1,3 +1,32 @@
-<div>
-    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
-</div>
+@extends('layouts.panel')
+@section('title', 'Horario/Actualizar')
+@section('content')
+
+    <div class="col-x1-12 order-x1-1">
+        <div class="card bg-secondary shadow">
+            <div class="card-header b-white border-0">
+                <div class="row align-items-center">
+                    <div class="col-8">
+                        <h3 class="mb-0"><i class="fas fa-plus-circle"></i> Actualizar Horario</h3>
+                    </div>
+
+                    <div class="col-4 text-right">
+                        <a href="{{ route('horaries.index') }}" class="btn btn-sm btn-primary">
+                            <i class="fas fa-arrow-left"></i> Regresar
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body container-fluid">
+                <form action="{{ route('horaries.update', $horaries->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+
+                    @include('horaries.form')
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
